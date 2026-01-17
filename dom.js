a0402ba69536bbb34c49a457f957c416d5e204c5
Sydney Solomon <sydney.solomon60@gmail.com>
2026-01-17 11:28:16 -0700

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,31 @@
+const title = document.querySelector('h1');
+
+console.log(title);
+
+title.textContent ='Web Page Components';
+
+let topics = document.querySelector('topics');
+
+document.querySelector('#topics').style.color = 'red';
+
+document.getElementById('topics').style.color = 'purple';
+
+let list = document.querySelector('.list');
+
+list.style.border = '3px solid black';
+
+let para = document.querySelector('p');
+
+//para.style.backgroundColor ='lightblue';
+
+para.classList.add('background');
+
+//document.querySelector('body').classList.add('background');
+
+
+let selectElem = document.getElementById('webdevlist');
+selectElem.addEventListener('change', function(){
+    let codeValue = selectElem.value;
+    console.log(codeValue);
+})
+                
